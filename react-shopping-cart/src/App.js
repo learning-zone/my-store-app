34e25b9c982a90fb1fcab8a35b472d3dac37bffe
Pradeep Kumar <pradeep.vwa@gmail.com>
2021-09-06 10:05:22 +0530

    React Shopping Cart

---
+++ react-shopping-cart/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shooping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
