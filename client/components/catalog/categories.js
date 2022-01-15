d5e327ffe22561b7ff0c43b9e2dd049a7ad5aba1
Pradeep Kumar <pradeep.vwa@gmail.com>
2022-01-15 11:16:21 +0530

    Create categories.js

---
+++ client/components/catalog/categories.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class categories extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Categories Page</h1>
+            </div>
+        )
+    }
+}
