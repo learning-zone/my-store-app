0d686e3ccb5886170d1639ce5374d6b03bf9520f
Pradeep Kumar <pradeep.vwa@gmail.com>
2022-01-15 11:05:47 +0530

    Page Not Found

--- client/components/error/NotFound.js
+++ client/components/error/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ReportProblem } from '@material-ui/icons';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div style={{ margin: '10px' }}>
+      <h2>
+        <ReportProblem /> 404 - Page Not Found
+      </h2>
       <p>I'm sorry, the page you were looking for cannot be found!</p>
     </div>
   );
