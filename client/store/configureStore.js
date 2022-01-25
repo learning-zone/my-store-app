13487f850a0a82c6b083299dd3c38ddebf31dbcc
Pradeep Kumar <pradeep.vwa@gmail.com>
2022-01-25 19:58:20 +0530

    Update configureStore.js

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,18 +1,15 @@
-import {applyMiddleware, compose, createStore} from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import {routerMiddleware} from 'connected-react-router';
+import { routerMiddleware } from 'connected-react-router';
 import logger from 'redux-logger';
 
 import createRootReducer from '../reducers';
 import history from '../utils/history';
 
-export {history};
+export { history };
 
 const middlewares = [thunkMiddleware, logger, routerMiddleware(history)];
 
-const store = createStore(createRootReducer(history), {}, compose(
-    applyMiddleware(...middlewares)
-    )
-);
+const store = createStore(createRootReducer(history), {}, compose(applyMiddleware(...middlewares)));
 
 export default store;
