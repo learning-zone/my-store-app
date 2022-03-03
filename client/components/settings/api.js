6acefa48abb808c9d9388f0cf38ab728b97abc7c
Pradeep Kumar <pradeep.vwa@gmail.com>
2022-03-03 15:34:11 +0530

    Update api.js

--- client/components/settings/api.js
+++ client/components/settings/api.js
@@ -1,9 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { DataGrid } from '@mui/x-data-grid';
-import { API_URL } from '../../config/config';
 import _ from 'underscore';
 import * as moment from 'moment';
+
+import { DataGrid } from '@mui/x-data-grid';
+import { API_URL } from '../../config/config';
+
+import EditIcon from '@mui/icons-material/Edit';
+import { FormControlLabel, IconButton } from '@mui/material';
+
+const MatEdit = ({ index }) => {
+  const handleEditClick = () => {
+    console.log(index);
+  };
+
+  return (
+    <FormControlLabel
+      control={
+        <IconButton color="primary" aria-label="Edit Record" onClick={handleEditClick}>
+          <EditIcon />
+        </IconButton>
+      }
+      label=""
+    />
+  );
+};
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 90 },
@@ -11,6 +32,22 @@ const columns = [
   { field: 'status', headerName: 'Status', width: 150, editable: true },
   { field: 'date_added', headerName: 'Date Added', type: 'date', width: 250, editable: true },
   { field: 'date_modified', headerName: 'Date Modified', type: 'date', width: 250, editable: true },
+  {
+    field: 'actions',
+    type: 'actions',
+    headerName: 'Actions',
+    width: 100,
+    renderCell: (params) => {
+      return (
+        <div
+          className="d-flex justify-content-between align-items-center"
+          style={{ cursor: 'pointer' }}
+        >
+          <MatEdit index={params.row.id} />
+        </div>
+      );
+    },
+  },
 ];
 
 export default function API() {
